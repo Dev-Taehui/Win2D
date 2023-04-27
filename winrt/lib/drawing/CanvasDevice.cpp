aaf0f349460c777e9aadf7f50e0532d362801157
Sergio Pedri <sergiopedri@microsoft.com>
2023-04-27 12:54:29 +0000

    Merged PR 25566: Minor tweaks from external testing
    This PR includes a couple of very small tweaks for things I noticed while testing things externally.
    Specifically, this PR mostly has two changes:
    - Return `D2DERR_WRONG_FACTORY` when a device factory is detected (instead of `E_INVALIDARG`). This is much more specific, and can help callers better understand what's going wrong instead of just guessing.
    - Renamed some internal helpers (eg. `Add` -> `RegisterWrapper`) for consistency with the public header.

--- winrt/lib/drawing/CanvasDevice.cpp
+++ winrt/lib/drawing/CanvasDevice.cpp
@@ -479,7 +479,7 @@ namespace ABI { namespace Microsoft { namespace Graphics { namespace Canvas
                     ThrowHR(E_INVALIDARG);
                 }
 
-                wasAdded = ResourceManager::TryAdd(resource, wrapper);
+                wasAdded = ResourceManager::TryRegisterWrapper(resource, wrapper);
             });
 
         if (hresult == S_OK)
@@ -505,7 +505,7 @@ namespace ABI { namespace Microsoft { namespace Graphics { namespace Canvas
                     ThrowHR(E_INVALIDARG);
                 }
 
-                wasRemoved = ResourceManager::TryRemove(resource);
+                wasRemoved = ResourceManager::TryUnregisterWrapper(resource);
             });
 
         if (hresult == S_OK)
